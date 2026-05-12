fff3c0ae7d25c550b5ef370afa0930137dac0a22
Mukul Tiwari <mukultiwari6773@gmail.com>
2026-05-12 14:43:28 +0530

    fixed cloudinary production upload

--- server.js
+++ server.js
@@ -35,19 +35,11 @@ mongoose.connect(process.env.MONGO_URI)
 const multer = require("multer");
 
 const storage = multer.memoryStorage(); // ✅ store in memory
+
 const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB
-  },
-  fileFilter: (req, file, cb) => {
-    const allowed = ["image/jpeg", "image/png", "image/jpg"];
-
-    if (allowed.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only JPG, JPEG, PNG allowed"));
-    }
+    fileSize: 5 * 1024 * 1024
   }
 });
 
